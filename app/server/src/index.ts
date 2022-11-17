6a8016b3bc2f306e0af619ad3770264a8145b455
Dave136 <davejs136@gmail.com>
2022-11-17 12:44:59 -0400

    feat(server): generate upload folder when is initialized

--- app/server/src/index.ts
+++ app/server/src/index.ts
@@ -4,11 +4,14 @@ import multer from 'fastify-multer';
 import fastifyStatic from '@fastify/static';
 import * as path from 'path';
 import routes from './routes';
+import { generateUploadFolder } from './utils';
 
 (async () => {
   const app = fastify();
   const host = '0.0.0.0';
   const port = +process.env.PORT || 5000;
+
+  generateUploadFolder();
 
   app.register(cors);
   app.register(multer.contentParser);
