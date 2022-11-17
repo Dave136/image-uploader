08c24b32f9e0ea60c9f614840225b73ba394a593
Dave136 <davejs136@gmail.com>
2022-11-17 00:42:09 -0400

    fix: upload-success classes

--- app/client/src/components/upload-success.tsx
+++ app/client/src/components/upload-success.tsx
@@ -20,13 +20,13 @@ const UploadSuccess = () => {
           Uploaded Successfully!
         </h1>
         <UploadedImage />
-        <div className="mt-[1.5625rem] max-w-[21.125rem] mb-[2.0625rem]">
+        <div className="mt-[1.5625rem] min-w-[21.125rem] mb-[2.0625rem]">
           <div className="border border-custom-color-1 bg-custom-color-2 h-[2.125rem] rounded-lg w-full flex items-center p-2 pr-0 dark:bg-zinc-600 dark:border-zinc-700">
-            <p className="truncate text-[.5rem] w-[80%] pr-2 text-custom-color">
+            <p className="truncate text-[.5rem] w-full pr-2 text-custom-color dark:text-gray-200">
               {uploadedPath}
             </p>
             <button
-              className="bg-primary text-white p-3 w-[20%] h-[1.875rem] rounded-lg text-[.5rem] flex justify-center transition ease items-center hover:bg-primary-hover"
+              className="bg-primary text-white p-3 min-w-[4rem] h-[1.875rem] rounded-lg text-[.5rem] flex justify-center transition ease items-center hover:bg-primary-hover"
               onClick={setCopied}
               disabled={isCopied}
             >
